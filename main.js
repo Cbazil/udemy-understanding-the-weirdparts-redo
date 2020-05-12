2fa8fd30ac9816d4afe6f7a8c5aff56fddf7fa2b
Cbazil <chadbosch08@gmail.com>
2020-05-13 01:46:57 +0200

    learning session init

---
+++ main.js
@@ -0,0 +1,42 @@
+/*
+function a(){
+  function b() {
+    console.log(myVar);
+  }
+  var myVar = 2;
+  b();
+}
+
+var myVar = 1;
+a();
+b();
+*/
+
+
+/*
+var a = 1 + '2';
+console.log(a); */
+
+/* console.log(1 < 2 < 3);*/
+
+/* Objects and the dot
+var person = new Object();
+
+person["firstname"] = "John";
+person.surname = "Smith";
+
+person.address = new Object();
+person.address.street = "14 Cradock Rd";
+person["address"]["suburb"] = "Steenberg";
+person.address.city = "Cape Town";
+
+console.log(person);
+
+*/
+
+// primative, objects and functions
+
+var person ={ firstname: "John", surname: "Doe"};
+// Object literal 
+
+console.log(person);
