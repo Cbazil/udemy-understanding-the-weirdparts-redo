f644c8b562786b36de26c03dd798b9bc06a9cbed
Cbazil <chadbosch08@gmail.com>
2020-05-13 23:20:52 +0200

    statements and expressions

--- main.js
+++ main.js
@@ -36,7 +36,55 @@ console.log(person);
 
 // primative, objects and functions
 
+/*
 var person ={ firstname: "John", surname: "Doe"};
 // Object literal 
 
-console.log(person);
+var english = {};
+var spanish = {};
+
+english.greet = "Hello";
+spanish.greet = "Hola";
+
+console.log(spanish.greet);
+*/
+
+// A function is a special function that contains code
+/*
+function greeting(){
+  console.log("Hello");
+}
+
+greeting.myName = "Chad";
+
+console.log(greeting.myName);
+*/
+
+// function statement
+/*
+greet(); // looks at memory at its address
+
+function greet(){
+  console.log("hi");
+}
+*/
+
+// Stores code in memory (Hoisting)
+
+// function expression
+/*
+var anonymousGreet = function() {
+  console.log("hi");
+} // equal sign expression creates a function object
+
+anonymousGreet(); // returns an object 
+
+*/
+
+function log(a) {
+  a();
+}
+
+log(function test(){
+  console.log("Hello func");
+});
