a613e7fb0823448861765bfda11205af518b66fa
Cbazil <chadbosch08@gmail.com>
2020-05-19 21:20:35 +0200

    currying play

--- main.js
+++ main.js
@@ -434,3 +434,6 @@ function multiply(a, b){
 
 var multiplyByTwo = multiply.bind(this, 2);
 console.log(multiplyByTwo(4));
+
+var multiplyByThree = multiply.bind(this, 3);
+console.log(multiplyByThree(6));
